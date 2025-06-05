b5c661e1f5a18e35492fe759b7f0397784494644
GJWilLRie <49706256+GJWilLRie@users.noreply.github.com>
2025-06-05 16:32:52 +1000

    strips(or wut ever it is) Added

--- 9103 final/sketch.js
+++ 9103 final/sketch.js
@@ -1,5 +1,317 @@
 let rings = [];
 let ringConfigs = [];
+let fillColors = ['#FABC08','#4CAECD','#06978A','#D70E08'];
+
+let ellipses = [
+    { x: 286.8, y: 73.4, angle: 0, fillColor: null},
+    { x: 301.1, y: 40.6, angle: 0, fillColor: null},
+    { x: 293.2, y: 11.8, angle: 0, fillColor: null},
+    { x: 252.8, y: 115.8, angle: 0, fillColor: null},
+    { x: 254.4, y: 135.9, angle: 0, fillColor: null},
+    { x: 360.2, y: 251.2, angle: 0, fillColor: null},
+    { x: 365.4, y: 268.9, angle: 0, fillColor: null},
+    { x: 369.4, y: 291.9, angle: 0, fillColor: null},
+    { x: 262.5, y: 188.7, angle: 0, fillColor: null},
+    { x: 282.7, y: 208.4, angle: 0, fillColor: null},
+    { x: 375.3, y: 204.6, angle: 0, fillColor: null},
+    { x: 396.3, y: 187.6, angle: 0, fillColor: null},
+    { x: 417.6, y: 160.2, angle: 0, fillColor: null},
+    { x: 414.9, y: 143.6, angle: 0, fillColor: null},
+    { x: 409.5, y: 126.6, angle: 0, fillColor: null},
+    { x: 403.5, y: 96.6, angle: 0, fillColor: null},
+    { x: 406.1, y: 68.3, angle: 0, fillColor: null},
+    { x: 425.5, y: 48.0, angle: 0, fillColor: null},
+    { x: 446.5, y: 32.0, angle: 0, fillColor: null},
+    { x: 481.4, y: 9.7, angle: 0, fillColor: null},
+    { x: 482.9, y: 21.4, angle: 0, fillColor: null},
+    { x: 252.6, y: 216.3, angle: 0, fillColor: null},
+    { x: 462.6, y: 326.3, angle: 0, fillColor: null},
+    { x: 483.6, y: 308.3, angle: 0, fillColor: null},
+    { x: 55.6, y: 398.3, angle: 0, fillColor: null},
+    { x: 44.6, y: 407.3, angle: 0, fillColor: null},
+    { x: 31.2, y: 419.0, angle: 0, fillColor: null},
+    { x: 1.6, y: 460.4, angle: 0, fillColor: null},
+    { x: 1.5, y: 483.6, angle: 0, fillColor: null},
+    { x: 62.4, y: 367.7, angle: 0, fillColor: null},
+    { x: 59.3, y: 351.0, angle: 0, fillColor: null},
+    { x: 56.2, y: 334.1, angle: 0, fillColor: null},
+    { x: 52.4, y: 320.0, angle: 0, fillColor: null},
+    { x: 44.4, y: 306.4, angle: 0, fillColor: null},
+    { x: 9.1, y: 436.1, angle: 0, fillColor: null},
+    { x: 240.6, y: 228.3, angle: 0, fillColor: null},
+    { x: 286.6, y: 484.3, angle: 0, fillColor: null},
+    { x: 271.6, y: 499.3, angle: 0, fillColor: null},
+    { x: 251.5, y: 515.7, angle: 0, fillColor: null},
+    { x: 220.5, y: 516.7, angle: 0, fillColor: null},
+    { x: 198.5, y: 515.7, angle: 0, fillColor: null},
+    { x: 167.6, y: 505.2, angle: 0, fillColor: null},
+    { x: 155.8, y: 480.3, angle: 0, fillColor: null},
+    { x: 149.6, y: 461.6, angle: 0, fillColor: null},
+    { x: 141.6, y: 440.6, angle: 0, fillColor: null},
+    { x: 231.6, y: 240.3, angle: 0, fillColor: null},
+    { x: 365.6, y: 325.3, angle: 0, fillColor: null},
+    { x: 327.7, y: 353.7, angle: 0, fillColor: null},
+    { x: 345.4, y: 342.2, angle: 0, fillColor: null},
+    { x: 213.7, y: 263.9, angle: 0, fillColor: null},
+    { x: 213.4, y: 295.0, angle: 0, fillColor: null},
+    { x: 216.4, y: 309.8, angle: 0, fillColor: null},
+    { x: 236.5, y: 357.0, angle: 0, fillColor: null},
+    { x: 205.9, y: 362.2, angle: 0, fillColor: null},
+    { x: 197.1, y: 264.6, angle: 0, fillColor: null},
+    { x: 107.4, y: 201.8, angle: 0, fillColor: null},
+    { x: 15.6, y: 129.6, angle: 0, fillColor: null},
+    { x: 112.4, y: 217.8, angle: 0, fillColor: null},
+    { x: 104.9, y: 245.2, angle: 0, fillColor: null},
+    { x: 56.9, y: 305.3, angle: 0, fillColor: null},
+    { x: 71.1, y: 286.7, angle: 0, fillColor: null},
+    { x: 85.8, y: 270.4, angle: 0, fillColor: null},
+    { x: 103.5, y: 185.4, angle: 0, fillColor: null},
+    { x: 309.7, y: 213.9, angle: 0, fillColor: null},
+    { x: 440.7, y: 171.9, angle: 0, fillColor: null},
+    { x: 463.7, y: 177.9, angle: 0, fillColor: null},
+    { x: 484.7, y: 186.9, angle: 0, fillColor: null},
+    { x: 505.7, y: 195.9, angle: 0, fillColor: null},
+    { x: 91.7, y: 158.9, angle: 0, fillColor: null},
+    { x: 77.7, y: 152.9, angle: 0, fillColor: null},
+    { x: 60.9, y: 147.0, angle: 0, fillColor: null},
+    { x: 45.0, y: 142.7, angle: 0, fillColor: null},
+    { x: 322.7, y: 217.9, angle: 0, fillColor: null},
+    { x: 353.7, y: 226.9, angle: 0, fillColor: null},
+    { x: 295.9, y: 403.7, angle: 0, fillColor: null},
+    { x: 444.9, y: 371.7, angle: 0, fillColor: null},
+    { x: 452.9, y: 393.7, angle: 0, fillColor: null},
+    { x: 459.9, y: 414.7, angle: 0, fillColor: null},
+    { x: 302.3, y: 439.9, angle: 0, fillColor: null},
+    { x: 301.8, y: 439.8, angle: 0, fillColor: null},
+    { x: 305.8, y: 472.6, angle: 0, fillColor: null},
+    { x: 294.4, y: 378.8, angle: 0, fillColor: null},
+    { x: 274.8, y: 88.4, angle: 0, fillColor: null},
+    { x: 176.8, y: 100.4, angle: 0, fillColor: null},
+    { x: 262.8, y: 371.4, angle: 0, fillColor: null},
+    { x: 88.9, y: 394.4, angle: 0, fillColor: null},
+    { x: 118.8, y: 409.4, angle: 0, fillColor: null},
+    { x: 433.8, y: 342.4, angle: 0, fillColor: null},
+    { x: 408.8, y: 328.4, angle: 0, fillColor: null},
+    { x: 393.8, y: 323.4, angle: 0, fillColor: null},
+    { x: 277.8, y: 377.4, angle: 0, fillColor: null},
+    { x: 141.5, y: 32.6, angle: 0, fillColor: null},
+    { x: 145.4, y: 51.8, angle: 0, fillColor: null},
+    { x: 150.4, y: 67.8, angle: 0, fillColor: null},
+    { x: 147.6, y: 97.8, angle: 0, fillColor: null},
+    { x: 129.7, y: 122.8, angle: 0, fillColor: null},
+    { x: 118.7, y: 134.8, angle: 0, fillColor: null},
+    { x: 105.7, y: 150.8, angle: 0, fillColor: null},
+    { x: 194.8, y: 106.4, angle: 0, fillColor: null},
+    { x: 218.9, y: 111.4, angle: 0, fillColor: null},
+    { x: 318.7, y: 476.5, angle: 0, fillColor: null},
+    { x: 346.7, y: 477.5, angle: 0, fillColor: null},
+    { x: 370.5, y: 486.6, angle: 0, fillColor: null},
+    { x: 392.9, y: 487.3, angle: 0, fillColor: null},
+    { x: 422.0, y: 476.5, angle: 0, fillColor: null},
+    { x: 438.0, y: 461.5, angle: 0, fillColor: null},
+    { x: 463.0, y: 439.5, angle: 0, fillColor: null},
+    { x: 488.4, y: 444.6, angle: 0, fillColor: null},
+    { x: 242.5, y: 111.4, angle: 0, fillColor: null}
+];
+
+
+function fillEllipse(ellipsesList) {
+    let count = 0;
+    for (let i = 0; i < ellipsesList.length; i++) {
+        if (count > 3){
+            count = 0;
+        }
+        ellipsesList[i].fillColor = fillColors[count];
+        count++;
+    }
+}
+
+let circles = [
+    { x: 12.0, y: 9.0, r: 6.0},
+    { x: 12.0, y: 9.0, r: 6.0},
+    { x: 12.0, y: 9.0, r: 6.0},
+    { x: 137.0, y: 9.0, r: 6.0},
+    { x: 137.0, y: 9.0, r: 6.0},
+    { x: 137.0, y: 9.0, r: 6.0},
+    { x: 156.0, y: 82.0, r: 6.0},
+    { x: 156.0, y: 82.0, r: 6.0},
+    { x: 156.0, y: 82.0, r: 6.0},
+    { x: 269.0, y: 206.0, r: 6.0},
+    { x: 269.0, y: 206.0, r: 6.0},
+    { x: 269.0, y: 206.0, r: 6.0},
+    { x: 141.0, y: 110.0, r: 6.0},
+    { x: 141.0, y: 110.0, r: 6.0},
+    { x: 141.0, y: 110.0, r: 6.0},
+    { x: 252.0, y: 105.0, r: 6.0},
+    { x: 252.0, y: 105.0, r: 6.0},
+    { x: 252.0, y: 105.0, r: 6.0},
+    { x: 299.0, y: 63.0, r: 6.0},
+    { x: 299.0, y: 63.0, r: 6.0},
+    { x: 299.0, y: 63.0, r: 6.0},
+    { x: 392.0, y: 78.0, r: 6.0},
+    { x: 392.0, y: 78.0, r: 6.0},
+    { x: 392.0, y: 78.0, r: 6.0},
+    { x: 406.5, y: 113.5, r: 5.5},
+    { x: 406.5, y: 113.5, r: 5.5},
+    { x: 406.5, y: 113.5, r: 5.5},
+    { x: 508.0, y: 24.0, r: 6.0},
+    { x: 508.0, y: 24.0, r: 6.0},
+    { x: 508.0, y: 24.0, r: 6.0},
+    { x: 516.0, y: 205.0, r: 6.0},
+    { x: 516.0, y: 205.0, r: 6.0},
+    { x: 516.0, y: 205.0, r: 6.0},
+    { x: 512.0, y: 289.0, r: 6.0},
+    { x: 512.0, y: 289.0, r: 6.0},
+    { x: 512.0, y: 289.0, r: 6.0},
+    { x: 447.0, y: 350.0, r: 6.0},
+    { x: 447.0, y: 350.0, r: 6.0},
+    { x: 447.0, y: 350.0, r: 6.0},
+    { x: 476.0, y: 432.0, r: 6.0},
+    { x: 476.0, y: 432.0, r: 6.0},
+    { x: 476.0, y: 432.0, r: 6.0},
+    { x: 378.0, y: 316.0, r: 6.0},
+    { x: 378.0, y: 316.0, r: 6.0},
+    { x: 378.0, y: 316.0, r: 6.0},
+    { x: 354.5, y: 239.5, r: 4.5},
+    { x: 354.5, y: 239.5, r: 4.5},
+    { x: 354.5, y: 239.5, r: 4.5},
+    { x: 287.0, y: 392.0, r: 6.0},
+    { x: 287.0, y: 392.0, r: 6.0},
+    { x: 287.0, y: 392.0, r: 6.0},
+    { x: 220.0, y: 342.0, r: 6.0},
+    { x: 220.0, y: 342.0, r: 6.0},
+    { x: 220.0, y: 342.0, r: 6.0},
+    { x: 211.0, y: 284.0, r: 6.0},
+    { x: 211.0, y: 284.0, r: 6.0},
+    { x: 211.0, y: 284.0, r: 6.0},
+    { x: 219.0, y: 249.0, r: 6.0},
+    { x: 219.0, y: 249.0, r: 6.0},
+    { x: 219.0, y: 249.0, r: 6.0},
+    { x: 70.0, y: 388.0, r: 6.0},
+    { x: 70.0, y: 388.0, r: 6.0},
+    { x: 70.0, y: 388.0, r: 6.0},
+    { x: 129.0, y: 415.0, r: 6.0},
+    { x: 129.0, y: 415.0, r: 6.0},
+    { x: 129.0, y: 415.0, r: 6.0},
+    { x: 149.0, y: 403.0, r: 6.0},
+    { x: 149.0, y: 403.0, r: 6.0},
+    { x: 149.0, y: 403.0, r: 6.0},
+    { x: 306.0, y: 456.0, r: 6.0},
+    { x: 306.0, y: 456.0, r: 6.0},
+    { x: 306.0, y: 456.0, r: 6.0},
+    { x: 295.0, y: 473.0, r: 6.0},
+    { x: 295.0, y: 473.0, r: 6.0},
+    { x: 295.0, y: 473.0, r: 6.0},
+    { x: 421.5, y: 335.5, r: 4.5},
+    { x: 421.5, y: 335.5, r: 4.5},
+    { x: 421.5, y: 335.5, r: 4.5},
+    { x: 309.5, y: 366.5, r: 4.5},
+    { x: 309.5, y: 366.5, r: 4.5},
+    { x: 309.5, y: 366.5, r: 4.5},
+    { x: 243.5, y: 363.5, r: 4.5},
+    { x: 243.5, y: 363.5, r: 4.5},
+    { x: 243.5, y: 363.5, r: 4.5},
+    { x: 50.5, y: 295.5, r: 4.5},
+    { x: 50.5, y: 295.5, r: 4.5},
+    { x: 50.5, y: 295.5, r: 4.5},
+    { x: 114.5, y: 226.5, r: 4.5},
+    { x: 114.5, y: 226.5, r: 4.5},
+    { x: 114.5, y: 226.5, r: 4.5},
+    { x: 99.5, y: 255.5, r: 4.5},
+    { x: 99.5, y: 255.5, r: 4.5},
+    { x: 99.5, y: 255.5, r: 4.5},
+    { x: 187.5, y: 258.5, r: 4.5},
+    { x: 187.5, y: 258.5, r: 4.5},
+    { x: 187.5, y: 258.5, r: 4.5},
+    { x: 29.5, y: 138.5, r: 4.5},
+    { x: 29.5, y: 138.5, r: 4.5},
+    { x: 29.5, y: 138.5, r: 4.5},
+    { x: 101.5, y: 167.5, r: 4.5},
+    { x: 101.5, y: 167.5, r: 4.5},
+    { x: 101.5, y: 167.5, r: 4.5},
+    { x: 295.5, y: 209.5, r: 4.5},
+    { x: 295.5, y: 209.5, r: 4.5},
+    { x: 295.5, y: 209.5, r: 4.5},
+    { x: 337.5, y: 223.5, r: 4.5},
+    { x: 337.5, y: 223.5, r: 4.5},
+    { x: 337.5, y: 223.5, r: 4.5},
+    { x: 365.5, y: 214.5, r: 4.5},
+    { x: 365.5, y: 214.5, r: 4.5},
+    { x: 365.5, y: 214.5, r: 4.5},
+    { x: 430.5, y: 165.5, r: 4.5},
+    { x: 430.5, y: 165.5, r: 4.5},
+    { x: 430.5, y: 165.5, r: 4.5},
+    { x: 297.5, y: 25.5, r: 4.5},
+    { x: 297.5, y: 25.5, r: 4.5},
+    { x: 297.5, y: 25.5, r: 4.5},
+    { x: 406.5, y: 177.5, r: 4.5},
+    { x: 406.5, y: 177.5, r: 4.5},
+    { x: 406.5, y: 177.5, r: 4.5},
+    { x: 262.5, y: 175.5, r: 4.5},
+    { x: 262.5, y: 175.5, r: 4.5},
+    { x: 262.5, y: 175.5, r: 4.5},
+    { x: 256.5, y: 160.5, r: 4.5},
+    { x: 256.5, y: 160.5, r: 4.5},
+    { x: 256.5, y: 160.5, r: 4.5},
+    { x: 2.5, y: 112.5, r: 4.5},
+    { x: 2.5, y: 112.5, r: 4.5},
+    { x: 2.5, y: 112.5, r: 4.5},
+    { x: 26.5, y: 297.5, r: 4.5},
+    { x: 26.5, y: 297.5, r: 4.5},
+    { x: 26.5, y: 297.5, r: 4.5},
+    { x: 329.5, y: 474.5, r: 4.5},
+    { x: 329.5, y: 474.5, r: 4.5},
+    { x: 329.5, y: 474.5, r: 4.5},
+    { x: 262.5, y: 512.5, r: 4.5},
+    { x: 262.5, y: 512.5, r: 4.5},
+    { x: 262.5, y: 512.5, r: 4.5},
+    { x: 179.5, y: 514.5, r: 4.5},
+    { x: 179.5, y: 514.5, r: 4.5},
+    { x: 179.5, y: 514.5, r: 4.5},
+    { x: 154.5, y: 495.5, r: 4.5},
+    { x: 154.5, y: 495.5, r: 4.5},
+    { x: 154.5, y: 495.5, r: 4.5},
+    { x: 299.5, y: 423.5, r: 4.5},
+    { x: 299.5, y: 423.5, r: 4.5},
+    { x: 299.5, y: 423.5, r: 4.5},
+    { x: 451.5, y: 449.5, r: 4.5},
+    { x: 451.5, y: 449.5, r: 4.5},
+    { x: 451.5, y: 449.5, r: 4.5},
+    { x: 405.5, y: 490.5, r: 4.5},
+    { x: 405.5, y: 490.5, r: 4.5},
+    { x: 405.5, y: 490.5, r: 4.5},
+    { x: 99.5, y: 398.5, r: 4.5},
+    { x: 99.5, y: 398.5, r: 4.5},
+    { x: 99.5, y: 398.5, r: 4.5},
+    { x: 6.5, y: 449.5, r: 4.5},
+    { x: 6.5, y: 449.5, r: 4.5},
+    { x: 6.5, y: 449.5, r: 4.5},
+    { x: 21.5, y: 427.5, r: 4.5},
+    { x: 21.5, y: 427.5, r: 4.5},
+    { x: 21.5, y: 427.5, r: 4.5},
+    { x: 4.5, y: 507.5, r: 4.5},
+    { x: 4.5, y: 507.5, r: 4.5},
+    { x: 4.5, y: 507.5, r: 4.5},
+    { x: 462.0, y: 17.0, r: 6.0},
+    { x: 462.0, y: 17.0, r: 6.0},
+    { x: 462.0, y: 17.0, r: 6.0},
+    { x: 291.0, y: -1.0, r: 5.0},
+    { x: 291.0, y: -1.0, r: 5.0},
+    { x: 291.0, y: -1.0, r: 5.0},
+    { x: 232.5, y: 519.5, r: 4.5},
+    { x: 232.5, y: 519.5, r: 4.5},
+    { x: 232.5, y: 519.5, r: 4.5},
+];
+
+function drawCircles(circleList) {
+    stroke("#000000");
+    strokeWeight(4);
+
+    for (let c of circleList) {
+        fill('#FFFFFF');
+        ellipse(c.x, c.y, c.r * 2, c.r * 2);
+    }
+}
 
 function setup() {
     createCanvas(520, 520);
@@ -127,7 +439,7 @@ function setup() {
             hasCurve: true,
             angle: 0
         },{ //number 11
-            x: 430,
+            x: 440,
             y: 250,
             fillStyles: ['layered', 'layered', 'dots'],
             bgColors: [color('#566140'), color('#BE5AA9'), color('#EDFDF9')],
@@ -139,7 +451,7 @@ function setup() {
             hasCurve: false,
             angle: PI / 3
         },{ //number 12
-            x: 70,
+            x: 77,
             y: 480,
             fillStyles: ['layered', 'layered', 'dots'],
             bgColors: [color('#369D44'), color('#D455BC'), color('#FBFEF7')],
@@ -221,6 +533,21 @@ function draw() {
     for (let r of rings) {
         r.display();
     }
+    fillEllipse(ellipses);
+    for (let i = 0; i < ellipses.length; i++) {
+        let e = ellipses[i];
+        stroke("#F28633");
+        strokeWeight(1);
+        fill(e.fillColor || "white");        // 默认白色填充
+
+        push();
+        translate(e.x, e.y);
+        rotate(radians(e.angle));
+        ellipse(0, 0, 5, 11);
+        pop();
+    }
+    drawCircles(circles);
+
 }
 
 class RingPattern {
@@ -343,7 +670,6 @@ class RingPattern {
         for (let i = 0; i < ringCount; i++) {
             let r = lerp(rMin + margin, rMax - margin, i / (ringCount - 1));
 
-            // 动态计算点数密度：大圈更多点
             let baseCount = floor(r * 1);
             let count = baseCount + floor(random(-4, 4));
 
@@ -354,7 +680,7 @@ class RingPattern {
                 let x = this.x + effectiveR * cos(angle);
                 let y = this.y + effectiveR * sin(angle);
 
-                // ⬇️ 动态点大小（根据 effectiveR 比例线性变化）
+
                 let sizeFactor = map(effectiveR, rMin, rMax, rMin * 0.03, rMax * 0.018); // 你可调范围
                 let w = random(4, 6) * sizeFactor;
                 let h = random(3, 5) * sizeFactor;
